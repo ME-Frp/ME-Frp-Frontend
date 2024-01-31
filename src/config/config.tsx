2bcbb2433f1a3690242e266712eefa828cb0e266
Aehxy <ahmrcxy@gmail.com>
2024-01-31 23:35:30 +0800

    合并config 修复下载

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,4 +1,14 @@
-import config from '../../config.json';
+const config = {
+    "dev": {
+        "api": "http://127.0.0.1:8080/api/",
+        "title": "镜缘映射|Dev|"
+    },
+    "prod": {
+        "api": "https://api.mefrp.com/api/",
+        "title": "镜缘映射|"
+    }
+}
+
 let current = config.dev;
 
 if (process.env.NODE_ENV === 'production') {
