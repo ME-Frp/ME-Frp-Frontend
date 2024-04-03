58f17a7b44c557336b9ad242c0407e6ef0b61775
PH-MNB <165527813+PH-MNB@users.noreply.github.com>
2024-04-04 06:33:36 +0800

    Update home.tsx

--- pages/console/home.tsx
+++ pages/console/home.tsx
@@ -266,7 +266,6 @@ export default function UserProfileCard() {
                             })}
                         </CardContent>
                     </Card>
-                </Grid>
             </Grid>
 
         </Layout>
