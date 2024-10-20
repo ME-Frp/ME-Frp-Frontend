c7947a9ae1ada7916ab88c9733a83430d0a66603
Aehxy <im@aehxy.com>
2024-10-20 16:37:44 +0800

    [*]修复下载

--- pages/download.tsx
+++ pages/download.tsx
@@ -7,8 +7,8 @@ import { useState } from 'react';
 export default function DownloadPage() {
     const sourceUrls = [
         {
-            link: 'https://download.mefrp.com/',
-            name: '[境内]ME Frp 下载站'
+            link: 'https://ap-cn-cd.mos.mcserverx.com/packages/',
+            name: '[境内]镜缘网络 MOS 对象存储'
         }
     ];
 
